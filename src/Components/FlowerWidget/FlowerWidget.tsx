c21edd8dd53551be298373cafcac6c86b1817b7b
kamrankamrani <kamrankamrani.fard@yahoo.com>
2022-10-15 22:35:33 +0330

    added rain with css TODO: enable rain with redux

--- src/Components/FlowerWidget/FlowerWidget.tsx
+++ src/Components/FlowerWidget/FlowerWidget.tsx
@@ -1,9 +1,18 @@
 import "./Style/style.css";
 
 export default function FlowerWidget() {
+  const isRain = false;
   return (
     <div className="flower-widget-container">
       <div className="flower-box">
+        <div className={`flower-rain ${isRain ? "visible" : "hidden"}`}>
+          <span className={`${isRain ? "flower-rain-animation" : ""}`}></span>
+          <span className={`${isRain ? "flower-rain-animation" : ""}`}></span>
+          <span className={`${isRain ? "flower-rain-animation" : ""}`}></span>
+          <span className={`${isRain ? "flower-rain-animation" : ""}`}></span>
+          <span className={`${isRain ? "flower-rain-animation" : ""}`}></span>
+          <span className={`${isRain ? "flower-rain-animation" : ""}`}></span>
+        </div>
         <div className="flower-pot"></div>
         <div className="flower">
           <div>
