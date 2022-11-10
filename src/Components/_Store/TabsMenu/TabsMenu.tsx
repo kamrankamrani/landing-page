4ad0ab96811929e5dcaac6527a96b54d9f46da43
kamrankamrani <kamrankamrani.fard@yahoo.com>
2022-11-10 16:32:56 +0330

    adding tabs.

--- src/Components/_Store/TabsMenu/TabsMenu.tsx
+++ src/Components/_Store/TabsMenu/TabsMenu.tsx
@@ -1,24 +1,46 @@
 import { Grid, Tabs, Tab, Typography } from "@mui/material";
+import { useState } from "react";
+import DetailsTab from "../DetailsTab/DetailsTab";
 import "./Style/style.css";
 
 export default function TabsMenu() {
+  const [tabSelected, setTabSelected] = useState(0);
+
+  const handleTabClick = (value: number) => {
+    //0 : مشخصات فنی
+    //1 : اپلیکیشن
+    //2 : پرسش و پاسخ
+    setTabSelected(value);
+  };
+
   return (
     <div className="tabs-container">
       <Grid container className="tabs">
         <Tabs>
           <Tab
-            className="tab-header"
+            disableTouchRipple
+            onClick={() => handleTabClick(0)}
+            className={`tab-header ${tabSelected === 0 ? "selected" : ""}`}
             label={<Typography>مشخصات فنی</Typography>}
           />
           <Tab
-            className="tab-header"
+            disableTouchRipple
+            onClick={() => handleTabClick(1)}
+            className={`tab-header ${tabSelected === 1 ? "selected" : ""}`}
             label={<Typography>اپلیکیشن</Typography>}
           />
           <Tab
-            className="tab-header"
+            disableTouchRipple
+            onClick={() => handleTabClick(2)}
+            className={`tab-header ${tabSelected === 2 ? "selected" : ""}`}
             label={<Typography>پرسش و پاسخ</Typography>}
           />
         </Tabs>
+        <Grid container className="tab-panel-container">
+          {tabSelected === 0 && <DetailsTab />}
+          {tabSelected === 1 && <Typography>App</Typography>}
+          {tabSelected === 2 && <Typography>Q&A</Typography>}
+        </Grid>
       </Grid>
     </div>
   );
